853118f5cda244b6e22f6eaddbeb148315a2e2e5
anandl-cuelogic <anandl@cuelogic.com>
2017-06-25 09:03:42 +0530

    starting project of auto layout

---
+++ AutoLayout/AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Anand Lilhare on 25/06/17.
+//  Copyright © 2017 Anand Lilhare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Anand Lilhare on 25/06/17.
+//  Copyright © 2017 Anand Lilhare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
