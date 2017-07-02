7f4056199bc8d939f65c1f097362da0bdc902f89
anandl-cuelogic <anandl@cuelogic.com>
2017-07-02 18:10:27 +0530

    Demo app of view and animation

---
+++ Uiview_and_Animation/Uiview_and_Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Uiview_and_Animation
+//
+//  Created by Anand Lilhare on 02/07/17.
+//  Copyright © 2017 Anand Lilhare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Uiview_and_Animation/Uiview_and_Animation/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Uiview_and_Animation
+//
+//  Created by Anand Lilhare on 02/07/17.
+//  Copyright © 2017 Anand Lilhare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (nonatomic) float scaleFactor;
+@property (nonatomic) float angle;
+@property (strong, nonatomic) UIView *boxView;
+@end
+
